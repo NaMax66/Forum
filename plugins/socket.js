b98f35819deec74af7bb3b9ff59a7c9980a66854
Максим Найдович <max.naid@mail.ru>
2020-05-31 17:30:13 +0500

    add fix

--- plugins/socket.js
+++ plugins/socket.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueSocketIO from 'vue-socket.io'
+const { APP_PORT, APP_IP } = process.env;
 
 export default function({store}) {
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: `http://${process.env.APP_IP}:${process.env.APP_PORT}`,
+    connection: `http://${APP_IP}:${APP_PORT}`,
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
