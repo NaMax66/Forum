8559e085de2c07eddd36a47d8740a096ca081dbf
Максим Найдович <max.naid@mail.ru>
2020-05-31 17:57:18 +0500

    fix build

--- plugins/socket.js
+++ plugins/socket.js
@@ -4,7 +4,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function({store}) {
   Vue.use(new VueSocketIO({
     debug: false,
-    connection: `http://${process.env.APP_IP}:${process.env.APP_PORT}`,
+    connection: `https://forum1.na4u.ru/`,
     vuex: {
       store,
       actionPrefix: 'SOCKET_',
